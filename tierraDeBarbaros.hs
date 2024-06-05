d63d043f84bfda278fd5b0d8cefc3a2e2a468803
dani-ojeda003 <166971419+dani-ojeda003@users.noreply.github.com>
2024-06-04 23:52:09 -0300

    Update tierraDeBarbaros.hs

--- tierraDeBarbaros.hs
+++ tierraDeBarbaros.hs
@@ -17,7 +17,7 @@ data Barbaro= UnBarbaro{
 dave :: Barbaro
 dave = UnBarbaro "Dave" 100 ["tejer","escribirPoesia"] [ardilla, varitasDefectuosas ]
 
---Punto 1.1--
+--Punto 1.1 Las espadas aumentan la fuerza de los bárbaros en 2 unidades por cada kilogramo de peso.--
 espada:: Peso->Barbaro->Barbaro
 espada unPeso = modificarFuerzaBarbaro unPeso  
 
@@ -27,30 +27,30 @@ modificarFuerzaBarbaro unPeso unBarbaro= unBarbaro {fuerza= (fuerza unBarbaro) +
 modificarNombre :: (String->String)->Barbaro->Barbaro
 modificarNombre unaFuncion unBarbaro= unBarbaro {nombre= unaFuncion.nombre$unBarbaro}
 
---Punto 1.2--
+--Punto 1.2 Los amuletosMisticos puerco-marranos otorgan una habilidad dada a un bárbaro.--
 amuletoMistico :: Habilidad->Objeto
 amuletoMistico unaHabilidad unBarbaro= modificarHabilidad ((++)[unaHabilidad]) unBarbaro
 
 
 
---1.3--
+--1.3 Las varitasDefectuosas, añaden la habilidad de hacer magia, pero desaparecen todos los demás objetos del bárbaro.--
 varitasDefectuosas:: Objeto 
 varitasDefectuosas unBarbaro= modificarHabilidad "Magia".vaciarObjetos$ unBarbaro
 
 vaciarObjetos:: Objeto
 vaciarObjetos unBarbaro= unBarbaro{objetos=[]}
 
---1.4--
+--1.4 Una ardilla, que no hace nada.--
 ardilla :: Objeto
 ardilla unBarbaro= unBarbaro
 
 
---1.5--
+--1.5  Una cuerda, que combina dos objetos distintos,obteniendo uno que realiza las transformaciones de los otros dos. --
 cuerda :: Objeto->Objeto->Objeto
 cuerda unObjeto otroObjeto unBarbaro= unObjeto.otroObjeto $ unBarbaro
 
 
---2--
+--2  El megafono es un objeto que potencia al bárbaro, concatenando sus habilidades y poniéndolas en mayúsculas. Sabiendo esto, definir al megafono, y al objeto megafonoBarbarico, que está formado por una cuerda, una ardilla y un megáfono. --
 
 megafono :: Objeto
 megafono unBarbaro= modificarHabilidad (ponerMayusculas.concatenar) unBarbaro
@@ -72,7 +72,26 @@ modificarHabilidad unaFuncion unBarbaro= unBarbaro{habilidades= unaFuncion.habil
 megafonoBarbarico :: Objeto
 megafonoBarbarico unBarbaro= cuerda ardilla megafono $ unBarbaro
 
---3--
+--3-- 
+{-
+
+Los bárbaros suelen ir de aventuras por el reino luchando contra las fuerzas del mal, pero ahora que tienen nuestra ayuda, quieren que se les diga si un grupo de bárbaros puede sobrevivir a cierta aventura.  Una aventura se compone de uno o más eventos, por ejemplo:
+
+invasionDeSuciosDuendes: Un bárbaro sobrevive si sabe “Escribir Poesía Atroz”
+cremalleraDelTiempo: Un bárbaro sobrevive si no tiene pulgares. Los bárbaros llamados Faffy y Astro no tienen pulgares, los demás sí. 
+ritualDeFechorias: Un bárbaro puede sobrevivir si pasa una o más pruebas como las siguientes: 
+saqueo: El bárbaro debe tener la habilidad de robar y tener más de 80 de fuerza.
+gritoDeGuerra: El bárbaro debe tener un poder de grito de guerra igual a la cantidad de letras de sus habilidades. El poder necesario para aprobar es 4 veces la cantidad de objetos del bárbaro.
+caligrafia: El bárbaro tiene caligrafía perfecta (para el estándar barbárico de la época) si sus habilidades contienen más de 3 vocales y comienzan con mayúscula.
+
+Sabiendo esto, se pide:
+Definir los eventos, modelar las aventuras y dar un ejemplo. 
+Definir la función sobrevivientes que tome una lista de bárbaros y una aventura, y diga cuáles bárbaros la sobreviven (es decir, pasan todas las pruebas)
+
+
+
+
+-}
 type Aventura = Barbaro->Bool
 
 invasionDeSuciosDuendes :: Aventura
@@ -136,6 +155,26 @@ sobrevivientes :: [Barbaro]->Aventura->[Barbaro]
 sobrevivientes unosBarbaros unaAventura= filter (\ unBarbaro-> pasaUnaAventura all unBarbaro unaAventura ) unosBarbaros
 
 pasaUnaAventura criterio unBarbaro naAventura = criterio (\evento-> evento unBarbaro) unaAventura
+{- 
+A - Los bárbaros se marean cuando tienen varias habilidades iguales. Por todo esto, nos piden desarrollar una función que elimine los elementos repetidos de una lista (sin utilizar nub ni nubBy)
+
+> sinRepetidos [1,2,3,4,4,5,5,6,7]
+[1,2,3,4,5,6,7]
+
+Nota: Puede usarse recursividad para este punto.
+
+B - Los bárbaros son una raza muy orgullosa, tanto que quieren saber cómo van a ser sus descendientes y asegurarse de que los mismos reciban su legado.
+
+El descendiente de un bárbaro comparte su nombre, y un asterisco por cada generación. Por ejemplo "Dave*", "Dave**" , "Dave***" , etc. 
+
+Además, tienen en principio su mismo poder, habilidades sin repetidos, y los objetos de su padre, pero antes de pasar a la siguiente generación, utilizan (aplican sobre sí mismos) los objetos. Por ejemplo, el hijo de Dave será equivalente a:
+
+(ardilla.varitasDefectuosas) (Barbaro "Dave*" 100 ["tejer","escribirPoesia"] [ardilla, varitasDefectuosas])
+
+
+Definir la función descendientes, que dado un bárbaro nos de sus infinitos descendientes. 
+
+-}
 
 sinRepetidos []= []
 sinRepetidos (cabeza:cola)
